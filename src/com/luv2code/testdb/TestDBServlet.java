72d6fec84a79bb4d182f292553d5e70bb4a17997
roelcantu <roelhcantu@gmail.com>
2018-05-18 00:22:14 -0500

    Initial CheckIn.

---
+++ src/com/luv2code/testdb/TestDBServlet.java
@@ -0,0 +1,48 @@
+package com.luv2code.testdb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestDBServlet
+ */
+@WebServlet("/TestDBServlet")
+public class TestDBServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String driver = "com.mysql.jdbc.Driver";
+		String userName = "root";
+		String password = "password";
+		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false";
+		
+		try {
+			PrintWriter out = response.getWriter();
+			
+			out.println("Connection to database : " + jdbcUrl);
+
+			Class.forName(driver);
+			
+			Connection connection = DriverManager.getConnection(jdbcUrl, userName, password);
+			
+			out.println("Connection successful, ROEL CANTU");
+			
+			connection.close();
+		} catch(Exception ex) {
+			ex.printStackTrace();
+			throw new ServletException(ex);
+		}
+	}
+
+}
